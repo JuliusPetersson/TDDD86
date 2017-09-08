46c02c1f67246716e5dabcd5bdd194c1299bee67
julpe622 <julpe622@student.liu.se>
2017-09-08 11:42:36 +0200

    fixed stack and made it print. also outcast for neighbours

--- wordchain/src/wordchain.cpp
+++ wordchain/src/wordchain.cpp
@@ -7,29 +7,47 @@ using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
+vector<string> neighbours(const string& word){
+    vector<string> neighbours;
+    for (unsigned i = 0; i<word.size(); i++)
+    {
+        for (char j:ALPHABET)
+        {
+            string tempWord = word;
+            tempWord[i] = j;
+            neighbours.push_back(tempWord);
+        }
+    }
+    return neighbours;
+}
+
 
 int main() {
     cout << "Welcome to TDDD86 Word Chain." << endl;
     cout << "If you give me two English words, I will transform the" << endl;
-    cout << "first into the second by changing one letter at a time." << endl;
-    cout << endl;
+    cout << "first into the second by changing one letter at a timee." << endl;
 
     cout << "Please type two words: ";
-
     string word1, word2;
 
     cin >> word1;
     cin >> word2;
+
     queue<stack<string>> que;
     stack<string> workingStack;
     workingStack.push(word1);
     que.push(workingStack);
-    cout << workingStack.top()<<endl;
-    while (!que.empty()){
 
+    while (!que.empty())
+    {
         que.front();
         que.pop();
         if (workingStack.top()==word2){
+            workingStack.push(word2);
+            for (unsigned int i=0; i<workingStack.size(); i++)
+            cout << workingStack.top() << " ";
+        }else{
+
         }
     }
 

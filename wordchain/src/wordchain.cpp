4f2ad0fd1246d5c0b4411bc1268a0a444c9ebb92
julpe622 <julpe622@student.liu.se>
2017-09-12 09:57:15 +0200

    now display all the legit neighbours do not push them to queue

--- wordchain/src/wordchain.cpp
+++ wordchain/src/wordchain.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <queue>
 #include <stack>
+#include <algorithm>
+#include <set>
 using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
@@ -21,6 +23,16 @@ vector<string> getNeighbours(const string& word){
     return neighbours;
 }
 
+void generateDictionary(set<string>& dictionary){
+    string dictWord;
+    fstream file;
+    file.open("dictionary.txt");
+    while (getline(file, dictWord)){
+        dictionary.insert(dictWord);
+    }
+
+}
+
 
 int main() {
     cout << "Welcome to TDDD86 Word Chain." << endl;
@@ -28,45 +40,46 @@ int main() {
     cout << "first into the second by changing one letter at a timee." << endl;
 
     cout << "Please type two words: ";
-    string word1, word2;
 
+    string word1, word2;
     cin >> word1;
     cin >> word2;
 
+    set<string> dictionary;
+    generateDictionary(dictionary);
+
     queue<stack<string>> que;
     stack<string> workingStack;
+    set<string> usedWords;
     workingStack.push(word1);
     que.push(workingStack);
 
-    while (!que.empty())
-    {
+    while (!que.empty()){
         que.front();
         que.pop();
-        if (workingStack.top()==word2)
-        {
+        if (workingStack.top()==word2){
             workingStack.push(word2);
-            for (unsigned int i=0; i<workingStack.size(); i++)
-            cout << workingStack.top() << " ";
-        }else
-        {
+         } else {
             vector<string> neighbours = getNeighbours(word1);
-            for (vector<string>::iterator it = neighbours.begin(); it<neighbours.end(); it++)
-            {
-                fstream file;
-                file.open("dictionary.txt");
-                string dictWord;
-                while (getline(cin,dictWord)){
-                    if (!(*it == dictWord)){
-                        neighbours.erase(it);
+            vector<string> wordstack;
+
+                for (vector<string>::iterator it = neighbours.begin(); it < neighbours.end(); it++){
+
+                    if ((dictionary.count(*it)) && !(find(usedWords.begin(), usedWords.end(), *it) != usedWords.end())){
+                        wordstack.push_back(*it);
+                        usedWords.insert(*it);
                     }
-                }
 
-
+            }
+            for (unsigned int i=0 ; i<wordstack.size();i++){
+                cout << wordstack.at(i) << endl;
 
             }
 
+            }
         }
-    }
+
+
 
     return 0;
 }

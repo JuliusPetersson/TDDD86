b933c2eff5ebb064f8edca5ea22eb180b4eb3abc
vikpa137 <vikpa137@student.liu.se>
2017-09-13 14:54:17 +0200

    evilhaaaaaang

--- evilhangman/src/evilhangman.cpp
+++ evilhangman/src/evilhangman.cpp
@@ -1,11 +1,33 @@
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <set>
 using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
+void generateDictionary(set<string>& dictionary){
+    string dictWord;
+    fstream file;
+    file.open("dictionary.txt");
+    while (getline(file, dictWord)){
+        dictionary.insert(dictWord);
+    }
+
+}
+
 int main() {
     cout << "Welcome to Hangman." << endl;
+
+    set<string> dictionary;
+    generateDictionary(dictionary);
+
+    for (vector<string>::iterator it = neighbours.begin(); it < neighbours.end(); it++){
+
+    }
 
     // TODO: Finish the program!
 

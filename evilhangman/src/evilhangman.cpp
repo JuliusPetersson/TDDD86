930b16a61831fa27a2da28bb7fbd502113e6152b
vikpa137 <vikpa137@student.liu.se>
2017-09-14 15:32:04 +0200

    Evilhangman now supports finding words by lenght and seing if two words are the same family

--- evilhangman/src/evilhangman.cpp
+++ evilhangman/src/evilhangman.cpp
@@ -19,20 +19,63 @@ void generateDictionary(set<string>& dictionary){
 
 }
 
-void getWordsWLength(unsigned int length,const set<string>& dictionary, set<string>& result){
+void getWordsByLength(unsigned int length,const set<string>& dictionary, set<string>& result){
     for(string word : dictionary){
         if (word.length() == length)result.insert(word);
     }
 }
 
-int main() {
-    cout << "Welcome to Hangman." << endl;
+bool sameFamily(string word1, string word2, char letter){
+    if (word1.length() ==  word2.length()){
+        for(int i = 0; i < word1.length(); i++){
+            if (word1[i] == letter || (word2[i] == letter)){
+                if (!(word2[i] == letter && word1[i] == letter)){
 
+                    cout << "fel familj" << endl;
+                    return false;
+                }
+            }
+        }
+
+        cout << "sann" << endl;
+        return true;
+    }
+    else{
+        cout << "olika längd" << endl;
+        return false;
+    }
+}
+
+void sortByFamily(char guess, set<string>& words){
+    set<set<string> > families;
+
+
+    while (!words.empty()) {
+        set<string> family;
+        family.insert(*words.begin());
+        words.erase(*words.begin());
+
+        /*for(set<string>::iterator word = words.begin();word < words.end(); word++){
+        }*/
+    }
+}
+
+int main() {
     set<string> dictionary;
+    set<string> words;
+    int length;
+
     generateDictionary(dictionary);
 
+    cout << "Welcome to Hangman." << endl;
+    cout << "please enter length of word." << endl;
 
-    // TODO: Finish the program!
+    while(words.size() == 0){
+        cin >> length;
+        getWordsByLength(length, dictionary, words);
+    }
+
+    cout << sameFamily("Cooadd","hejaff", 'a') << endl;
 
     return 0;
 }

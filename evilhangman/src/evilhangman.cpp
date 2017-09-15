b6fb4911b6f53cd2a7ea0be107e9aaa445e58d14
Viktor <viktor.palm.vp@gmail.com>
2017-09-15 11:58:01 +0200

    evilhangman: progressing

--- evilhangman/src/evilhangman.cpp
+++ evilhangman/src/evilhangman.cpp
@@ -13,8 +13,13 @@ void generateDictionary(set<string>& dictionary){
     string dictWord;
     fstream file;
     file.open("dictionary.txt");
-    while (getline(file, dictWord)){
-        dictionary.insert(dictWord);
+    if (file.is_open()){
+	while (getline(file, dictWord)){
+	    dictionary.insert(dictWord);
+	}
+    }
+    else{
+	cout << "file not opened" << endl;
     }
 
 }
@@ -30,52 +35,68 @@ bool sameFamily(string word1, string word2, char letter){
         for(int i = 0; i < word1.length(); i++){
             if (word1[i] == letter || (word2[i] == letter)){
                 if (!(word2[i] == letter && word1[i] == letter)){
-
-                    cout << "fel familj" << endl;
                     return false;
                 }
             }
         }
 
-        cout << "sann" << endl;
         return true;
     }
     else{
-        cout << "olika längd" << endl;
         return false;
     }
 }
 
-void sortByFamily(char guess, set<string>& words){
-    set<set<string> > families;
-
-
+void sortByFamily(char guess, set<string>& words, set<set<string> > families){
     while (!words.empty()) {
         set<string> family;
-        family.insert(*words.begin());
-        words.erase(*words.begin());
+	string testWord = *words.begin();
+        family.insert(testWord);
+        words.erase(testWord);
 
-        /*for(set<string>::iterator word = words.begin();word < words.end(); word++){
-        }*/
+        for(set<string>::iterator word = words.begin();word != words.end(); word++){
+	  if (sameFamily(testWord, *word, guess)){
+	    family.insert(*word);
+	    words.erase(*word);
+	  }
+        }
     }
+}
+
+void longestFamily(set<string> words, const set<set<string> > families){
+    set<string> longest;
+
+    longest = *families.begin();
+    
+    for (set<set<string> >::iterator family = families.begin();family != families.end(); family++){
+	if(longest.size() < (*family).size()) longest = *family;
+    }
+
+    words = longest;
 }
 
 int main() {
     set<string> dictionary;
     set<string> words;
+    set<set<string> > families;
     int length;
 
     generateDictionary(dictionary);
 
     cout << "Welcome to Hangman." << endl;
     cout << "please enter length of word." << endl;
-
-    while(words.size() == 0){
-        cin >> length;
-        getWordsByLength(length, dictionary, words);
+    
+    cin >> length;
+    
+    getWordsByLength(length, dictionary, words);
+    sortByFamily('e', words, families);
+    
+    for (set<string> family : families){
+	for (string word : family){
+	    cout << word << ", ";
+	}
+	cout << endl;
     }
-
-    cout << sameFamily("Cooadd","hejaff", 'a') << endl;
 
     return 0;
 }

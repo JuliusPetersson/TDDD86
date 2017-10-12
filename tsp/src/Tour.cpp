bf14acd125dec0917cd040bf50374246721d0ee5
vikpa137 <vikpa137@student.liu.se>
2017-10-12 16:43:53 +0200

    viktor dunno change

--- tsp/src/Tour.cpp
+++ tsp/src/Tour.cpp
@@ -11,17 +11,46 @@
 
 Tour::Tour()
 {
-    // TODO: write this member
+    //Node* firstNode = nullptr;
+
+    Point p0(100.0,100.0);
+    Point p1(500.0,100.0);
+    Point p2(500.0,500.0);
+    Point p3(100.0,100.0);
+
+    Node node3(p3);
+    Node node2(p2,&node3);
+    Node node1(p1,&node2);
+    Node node0(p0,&node1);
+
+    node3.next = &node0;
+    std::cout << *(node3.next)<< endl;
+    Node* firstNode = &node0;
 }
 
 Tour::~Tour()
 {
     // TODO: write this member
+    delete firstNode;
 }
 
 void Tour::show()
 {
     // TODO: write this member
+
+    Node* currNode = firstNode->next;
+
+    std::cout << &firstNode << endl;
+    while(currNode != firstNode){
+        std::cout << "start while" << endl;
+
+        std::cout << &currNode << endl;
+        std::cout << "middle while" << endl;
+        currNode = currNode->next;
+        std::cout << "about to crash" << endl;
+    }
+    std::cout << &firstNode << endl;
+
 }
 
 void Tour::draw(QGraphicsScene *scene)
@@ -42,6 +71,9 @@ double Tour::distance()
 void Tour::insertNearest(Point p)
 {
     // TODO: write this member
+    if(firstNode == nullptr){
+        Node node(p);
+    }
 }
 
 void Tour::insertSmallest(Point p)

0fc263b0d6da6bad207695df22c964f46181cfde
julius Petersson <juliuspetersson97@gmail.com>
2017-10-19 13:21:09 +0200

    testing, do not use if not fixing.

--- tsp/src/Tour.cpp
+++ tsp/src/Tour.cpp
@@ -12,30 +12,30 @@
 
 Tour::Tour()
 {
-    //Node* firstNode = nullptr;
+    Node* firstNode = nullptr;
 
-    Point p0(100.0,100.0);
-    Point p1(500.0,100.0);
-    Point p2(500.0,500.0);
-    Point p3(100.0,500.0);
+    //Point p0(100.0,100.0);
+    //Point p1(500.0,100.0);
+    //Point p2(500.0,500.0);
+    //Point p3(100.0,500.0);
 
-    node3 = new Node(p3);
-    node2 = new Node(p2,node3);
-    node1 = new Node(p1,node2);
-    node0 = new Node(p0,node1);
+    //node3 = new Node(p3);
+    //node2 = new Node(p2,node3);
+    //node1 = new Node(p1,node2);
+    //node0 = new Node(p0,node1);
 
     //node2->next = node0;
-    node3->next = node0;
-    firstNode = node0;
+    //node3->next = node0;
+    //firstNode = node0;
 }
 
 Tour::~Tour()
 {
     delete firstNode;
-    delete node0;
-    delete node1;
-    delete node2;
-    delete node3;
+    //delete node0;
+    //delete node1;
+    //delete node2;
+    //delete node3;
 }
 
 void Tour::show()
@@ -114,24 +114,24 @@ void Tour::insertNearest(Point p)
     // TODO: write this member
     if(firstNode == nullptr){
         firstNode = new Node(p,nullptr);
-
     }else{
         Node* currNode = firstNode->next;
 
-        Node* closestNode = nullptr;
-        double cloestDistance;
+        Node* closestNode = firstNode;
+        double cloestDistance = 0;
 
         while(currNode != firstNode){
             double valueX = currNode->point.x - p.x;
             double valueY = currNode->point.y - p.y;
 
-            //Qr warning is wrong, if not set it will not try it since ==nullptr is true!
-            if ((closestNode == nullptr) | (hypot(valueX,valueY)< cloestDistance)){
+            if ((cloestDistance == 0) | (hypot(valueX,valueY) < cloestDistance)){
                 closestNode = currNode;
             }
             currNode = currNode->next;
 
         }
+        Node* inNode = new Node(p, closestNode->next);
+        closestNode->next = inNode;
     }
 }
 
--- tsp/src/tsp.cpp
+++ tsp/src/tsp.cpp
@@ -38,19 +38,18 @@ int main(int argc, char *argv[]) {
 
     // run insertion heuristic
     Tour tour;
-    //TODO remove this line below
-    tour.draw(scene);
-    //double x;
-    //double y;
-    //while (input >> x >> y) {
-    //    Point p(x, y);
-    //    tour.insertNearest(p);
+
+    double x;
+    double y;
+    while (input >> x >> y) {
+        Point p(x, y);
+        tour.insertNearest(p);
         //uncomment the 4 lines below to animate
         //tour.draw(scene);
         //std::chrono::milliseconds dura(50);
         //std::this_thread::sleep_for(dura);
         //a.processEvents();
-    //}
+    }
     input.close();
 
     // print tour to standard output

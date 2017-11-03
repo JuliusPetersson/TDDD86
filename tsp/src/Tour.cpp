ec2a1d49b47c3c753402a6bc599445839b31c5f1
julius Petersson <juliuspetersson97@gmail.com>
2017-11-03 18:06:54 +0100

    fixed some of the mem leak may be left, ask Filip

--- tsp/src/Tour.cpp
+++ tsp/src/Tour.cpp
@@ -1,9 +1,3 @@
-// This is the .cpp file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
-
 #include <iostream>
 #include "Tour.h"
 #include "Node.h"
@@ -17,38 +11,57 @@ Tour::Tour()
 
 Tour::~Tour()
 {
+    if (firstNode != nullptr){
+
+        Node* currNode = firstNode->next;
+
+        std::cout << *firstNode << endl;
+        while(currNode != firstNode){
+            delete currNode;
+
+            currNode = currNode->next;
+        }
+    }
     delete firstNode;
 }
 
 void Tour::show()
 {
-    Node* currNode = firstNode->next;
+    if (firstNode != nullptr){
 
-    std::cout << *firstNode << endl;
-    while(currNode != firstNode){
-        std::cout << *currNode << endl;
+        Node* currNode = firstNode->next;
 
-        currNode = currNode->next;
+        std::cout << *firstNode << endl;
+        while(currNode != firstNode){
+            std::cout << *currNode << endl;
+
+            currNode = currNode->next;
+        }
     }
 }
 
 void Tour::draw(QGraphicsScene *scene)
 {
-    Node* currNode = firstNode;
+    if (firstNode != nullptr){
+        Node* currNode = firstNode;
 
-    bool firstTime = true;
-    while(currNode != firstNode || firstTime){
-        firstTime = false;
-        currNode->point.draw(scene);
-        currNode->next->point.draw(scene);
-        currNode->point.drawTo(currNode->next->point,scene);
+        bool firstTime = true;
+        while(currNode != firstNode || firstTime){
+            firstTime = false;
+            currNode->point.draw(scene);
+            currNode->next->point.draw(scene);
+            currNode->point.drawTo(currNode->next->point,scene);
 
-        currNode = currNode->next;
+            currNode = currNode->next;
+        }
     }
 }
 
 int Tour::size()
 {
+    if (firstNode == nullptr){
+        return 0;
+    }
     Node* currNode = firstNode->next;
 
     if(currNode == nullptr){
@@ -66,6 +79,9 @@ int Tour::size()
 
 double Tour::distance()
 {
+    if (firstNode == nullptr){
+        return 0;
+    }
     Node* currNode = firstNode;
     double distance = 0;
     bool firstTime = true;
@@ -95,29 +111,29 @@ void Tour::insertNearest(Point p)
         firstNode = new Node(p);
         firstNode->next = firstNode;
     }else{
-            Node* currNode = firstNode;
+        Node* currNode = firstNode;
 
-            Node* closestNode = firstNode;
-            double cloestDistance = 0;
+        Node* closestNode = firstNode;
+        double cloestDistance = 0;
 
-            bool firstTime = true;
-            while((currNode != firstNode) | firstTime){
-                firstTime = false;
-                double valueX = currNode->point.x - p.x;
-                double valueY = currNode->point.y - p.y;
+        bool firstTime = true;
+        while((currNode != firstNode) | firstTime){
+            firstTime = false;
+            double valueX = currNode->point.x - p.x;
+            double valueY = currNode->point.y - p.y;
 
-                double currNodeDistance = hypot(valueX,valueY);
-                if ((cloestDistance == 0) | (currNodeDistance < cloestDistance)){
-                    closestNode = currNode;
-                    cloestDistance = currNodeDistance;
-                }
-                currNode = currNode->next;
-
+            double currNodeDistance = hypot(valueX,valueY);
+            if ((cloestDistance == 0) | (currNodeDistance < cloestDistance)){
+                closestNode = currNode;
+                cloestDistance = currNodeDistance;
             }
-            Node* inNode = new Node(p, closestNode->next);
-            closestNode->next = inNode;
+            currNode = currNode->next;
+
         }
+        Node* inNode = new Node(p, closestNode->next);
+        closestNode->next = inNode;
     }
+}
 
 
 void Tour::insertSmallest(Point p)
@@ -126,33 +142,33 @@ void Tour::insertSmallest(Point p)
         firstNode = new Node(p,nullptr);
         firstNode->next = firstNode;
     }else{
-            Node* currNode = firstNode;
+        Node* currNode = firstNode;
 
-            Node* closestNode = firstNode;
-            double leastRaise = 0;
+        Node* closestNode = firstNode;
+        double leastRaise = 0;
 
-            bool firstTime = true;
-            while((currNode != firstNode) | firstTime){
-                firstTime = false;
-                double valueX = currNode->point.x - p.x;
-                double valueY = currNode->point.y - p.y;
+        bool firstTime = true;
+        while((currNode != firstNode) | firstTime){
+            firstTime = false;
+            double valueX = currNode->point.x - p.x;
+            double valueY = currNode->point.y - p.y;
 
-                double nextValueX = currNode->next->point.x - p.x;
-                double nextValueY = currNode->next->point.y - p.y;
+            double nextValueX = currNode->next->point.x - p.x;
+            double nextValueY = currNode->next->point.y - p.y;
 
-                double currValueX = currNode->point.x - currNode->next->point.x;
-                double currValueY = currNode->point.y - currNode->next->point.y;
+            double currValueX = currNode->point.x - currNode->next->point.x;
+            double currValueY = currNode->point.y - currNode->next->point.y;
 
-                double currNodeRaise = hypot(valueX,valueY) + hypot(nextValueX,nextValueY) - hypot(currValueX,currValueY);
-                if ((leastRaise == 0) | (currNodeRaise < leastRaise)){
-                    closestNode = currNode;
-                    leastRaise = currNodeRaise;
-                }
-                currNode = currNode->next;
-
+            double currNodeRaise = hypot(valueX,valueY) + hypot(nextValueX,nextValueY) - hypot(currValueX,currValueY);
+            if ((leastRaise == 0) | (currNodeRaise < leastRaise)){
+                closestNode = currNode;
+                leastRaise = currNodeRaise;
             }
-            Node* inNode = new Node(p, closestNode->next);
-            closestNode->next = inNode;
+            currNode = currNode->next;
+
         }
+        Node* inNode = new Node(p, closestNode->next);
+        closestNode->next = inNode;
     }
+}
 

aece49709df85ddeef348aef2a19a059959a5b3f
vikpa137 <vikpa137@student.liu.se>
2017-12-17 12:20:38 +0100

    depth and breadth search both changed and are now finished

--- trailblazer/src/trailblazer.cpp
+++ trailblazer/src/trailblazer.cpp
@@ -2,6 +2,7 @@
 #include "trailblazer.h"
 #include "stack"
 #include "iostream"
+#include "queue"
 using namespace std;
 
 vector<Node *> helperDepthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
@@ -13,17 +14,17 @@ vector<Node *> helperDepthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* 
         path.push_back(start);
     }
     else{
-        for (Node* a: graph.getNeighbors(start)){
-            if(!(a->visited)){
-                a->previous = start;
-                path = helperDepthFirstSearch(graph, a, end);
+        for (Node* node: graph.getNeighbors(start)){
+            if(!(node->visited)){
+                node->previous = start;
+                path = helperDepthFirstSearch(graph, node, end);
 
                 if (path.size() > 0){
-                    path.push_back(start);
+                    path.insert(path.begin(),start);
                     break;
                 }
                 else{
-                    a->setColor(GRAY);
+                    node->setColor(GRAY);
                 }
             }
         }
@@ -40,15 +41,34 @@ vector<Node *> depthFirstSearch(BasicGraph &graph, Vertex *start, Vertex *end){
     return helperDepthFirstSearch(graph, start, end);
 }
 
-
-
-
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
+    graph.resetData();
+    vector<Node*> path;
+    queue<Node*> nodeQueue;
+    nodeQueue.push(start);
+
+    while(nodeQueue.size() > 0 && start != end){
+        start = nodeQueue.front();
+        nodeQueue.pop();
+
+        start->visited = true;
+        start->setColor(GREEN);
+
+        for(Node* node : graph.getNeighbors(start)){
+            if (!node->visited){
+                node->previous = start;
+                node->setColor(YELLOW);
+                nodeQueue.push(node);
+            }
+        }
+    }
+
+    while(start->previous != nullptr){
+        path.insert(path.begin(), start);
+        start = start->previous;
+    }
+
+    path.insert(path.begin(), start);
     return path;
 }
 
